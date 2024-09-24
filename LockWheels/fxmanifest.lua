3dfda3d294bf140c533fb58629dd21540e44557c
KA1N3D2024 <kaine.duncan@ou.ac.uk>
2024-09-24 18:11:57 +0100

    Add files via upload

---
+++ LockWheels/fxmanifest.lua
@@ -0,0 +1,8 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'KA1N3D Software FiveM' -- Optional, you can add your name here
+description 'Lock Wheels Script for FiveM'
+version '1.0.0'
+
+client_script 'client.lua'
